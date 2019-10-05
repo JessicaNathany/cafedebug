49dc899bbb5d8739ba9a02e2987cee849af254a2
Jéssica Nathany Carvalho Freitas <jnathany@outlook.com>
2019-10-05 20:34:23 -0300

    criação da tela Fale Conosco

--- Cafedebug.Test/Service/UsuarioServiceTest.cs
+++ Cafedebug.Test/Service/UsuarioServiceTest.cs
@@ -12,40 +12,7 @@ namespace Cafedebug.Test.Service
     [TestClass]
     public class UsuarioServiceTest
     {
-        /// <summary>
-        /// Verifica o perfil do usuário antes de realizar 
-        /// qualquer ação no sistema
-        /// </summary>
-        public void VerificaPersilUsuario()
-        {
-            var mockUsuarioService = new Mock<IUsuarioService>();
-
-        }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        [TestMethod]
-        public void TestMethod1()
-        {
-            //
-            // TODO: Add test logic here
-            //
-        }
+       
+        
     }
 }
