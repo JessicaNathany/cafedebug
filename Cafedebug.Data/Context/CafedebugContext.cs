526220840436a488de90cb21579f1c29ce0d7d50
Denis Souza <denyssouza1@hotmail.com>
2020-03-27 00:03:13 -0300

    Renomeando tables do identity e  gerando migrations das mesmas

--- Cafedebug.Data/Context/CafedebugContext.cs
+++ Cafedebug.Data/Context/CafedebugContext.cs
@@ -1,4 +1,5 @@
 ﻿using Cafedebug.Business.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
@@ -27,6 +28,14 @@ namespace Cafedebug.Data.Context
             foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
 
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityUser<int>>(b => b.ToTable("User"));
+            modelBuilder.Entity<IdentityUserClaim<int>>(b => b.ToTable("UserClaims"));
+            modelBuilder.Entity<IdentityUserLogin<int>>(b => { b.ToTable("UserLogin"); b.HasKey("UserId"); });
+            modelBuilder.Entity<IdentityUserToken<int>>(b => { b.ToTable("UserTokens"); b.HasKey("UserId"); });
+            modelBuilder.Entity<IdentityRole<int>>(b => b.ToTable("Roles"));
+            modelBuilder.Entity<IdentityRoleClaim<int>>(b => b.ToTable("RoleClaims"));
+            modelBuilder.Entity<IdentityUserRole<int>>(b => { b.ToTable("UserRoles"); b.HasKey("UserId"); });
         }
     }
 }
