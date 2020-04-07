b149187d96af36ec8615eebdeb092ab7ecab410b
carolfelix <carolinafelix.ana@gmail.com>
2020-04-07 19:04:15 -0300

    Divisão das Areas - Site e Administrador

---
+++ Cafedebug.Web/Areas/Administrador/Controllers/AdmController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cafedebug.Web.Areas.Adm.Controllers
+{
+    public class AdminController : Controller
+    {
+        public AdminController()
+        {
+            // do stuff
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+    }
